d8d2304a2c9017255e57e8b13c5fc30e322b1804
Jara Santamaria Martinez <jarasmar@gmail.com>
2021-10-26 15:10:22 +0100

    Add a reducer to handle all book functions

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -2,14 +2,17 @@ import React, { useContext } from "react";
 import { BookContext } from "../contexts/BookContext";
 
 const BookDetails = ({ book }) => {
-  const { removeBook } = useContext(BookContext);
+  const { dispatch } = useContext(BookContext);
   return (
     <li>
       <div>
         <div className="title">{book.title}</div>
         <div className="author">{book.author}</div>
       </div>
-      <button className="remove-btn" onClick={() => removeBook(book.id)}>
+      <button
+        className="remove-btn"
+        onClick={() => dispatch({ type: "REMOVE_BOOK", id: book.id })}
+      >
         Finished
       </button>
     </li>
--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,26 +1,13 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useReducer } from "react";
+import { bookReducer } from "../reducers/bookReducer";
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-  const [books, setBooks] = useState([
-    { title: "La sombra del viento", author: "Carlos Ruiz Zafon", id: 1 },
-    { title: "Momo", author: "Michael Ende", id: 2 },
-  ]);
-
-  const addBook = (title, author) => {
-    // setBooks([...books, {title:title, author:author, id:books.length}])
-    // Shorter expression
-    setBooks([...books, { title, author, id: books.length }]);
-  };
-
-  const removeBook = (id) => {
-    // Keep in the array if the id of the current book in the iteration loop is not the passed id
-    setBooks(books.filter((book) => book.id !== id));
-  };
+  const [books, dispatch] = useReducer(bookReducer, []);
 
   return (
-    <BookContext.Provider value={{ books, addBook, removeBook }}>
+    <BookContext.Provider value={{ books, dispatch }}>
       {props.children}
     </BookContext.Provider>
   );
